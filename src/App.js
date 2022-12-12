14ba963485061d65a8e6adbf1e65f4277afa47cc
이태형 <spdhsrnvl123@naver.com>
2022-12-12 21:20:11 +0900

    🎉영단어 프로젝트 시작

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import DayList from "./component/DayList";
+import Header from "./component/Header";
+import Day from "./component/Day"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import EmptyPage from "./component/EmptyPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />}>
+          </Route>
+          <Route path ="/day/:day" element={<Day />}></Route>
+          <Route path= "*" element={<EmptyPage />}>
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
