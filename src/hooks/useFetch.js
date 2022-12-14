512e3e80809557b1eb2715e9eb99fcad60e39aa7
이태형 <spdhsrnvl123@naver.com>
2022-12-14 22:00:55 +0900

    ✨fetch Hook 구현

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 
-export default function useFetch(url){
-    const [data, setData] = useState([]);
+export default function useFetch(url) {
+  const [data, setData] = useState([]);
 
-    useEffect(()=>{
-        fetch(url)
-        .then(res =>{
-            return res.json();
-        })
-        .then(data =>{
-            setData(data);
-        },[url])
-    })
+  useEffect(() => {
+    fetch(url)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setData(data);
+      });
+  }, [url]);
 
-    return data;
+  return data;
 }
